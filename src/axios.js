45532bb9929b5722b089c74183fed925e4bff7c1
sebastian <sebastian99_19@hotmail.com>
2022-08-09 23:24:01 -0500

    change base url

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,8 @@ const instance = axios.create({
     // local
     // baseURL: "http://localhost:5000/api/"
     // Deploy in AWS
-    baseURL: "http://erptelecomunications-env.eba-fjcmfbiy.us-east-2.elasticbeanstalk.com/api/"
+    // baseURL: "http://erptelecomunications-env.eba-fjcmfbiy.us-east-2.elasticbeanstalk.com/api/"
+    baseURL: "https://app-erptelecomunications.herokuapp.com/api/"
 });
 
 export default instance;
